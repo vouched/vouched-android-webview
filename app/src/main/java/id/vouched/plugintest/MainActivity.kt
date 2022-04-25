03c8f8af919dd529d4e166411d02b34f6b33cb31
Jay Lorenzo <jay.lorenzo@vouched.id>
2022-04-24 19:46:54 -0700

    Some cleanup

--- app/src/main/java/id/vouched/plugintest/MainActivity.kt
+++ app/src/main/java/id/vouched/plugintest/MainActivity.kt
@@ -20,10 +20,10 @@ class MainActivity : AppCompatActivity() {
 
     private var cameraPermission: PermissionRequest? = null
     private val CAMERA_PERMISSION_REQUEST = 101299
-    //the vouched app key you are using
-    private val appId = "<Add your app public key here>"
-    // this points to the demo flow in production,
-    // replace this with whatever url you are using
+    //add your app key here
+    private val appId = "<APP_KEY>"
+    //the app url - this points to the Vouched production demo
+    //if hosting your own endpoint, change it accordingly
     private val webappUrl = "https://static.vouched.id/widget/demo/index.html#/demo?recognizeIDThreshold=0.81&cardIDThreshold=0.2&generalThreshold=0.9&glareQualityThreshold=0.6&qualityThreshold=0.6&selfieThreshold=0&holdSteadyIntervalFace=1250&detectorRunFrameInterval=2&stepTitles%5BFrontId%5D=Upload%20ID&stepTitles%5BFace%5D=Upload%20Headshot&stepTitles%5BDone%5D=Finished&stepTitles%5BID_Captured%5D=ID%20Captured&stepTitles%5BFace_Captured%5D=Face%20Captured&stepTitles%5BStart%5D=Start&stepTitles%5BBackId%5D=ID%20%28Back%29&content%5BcrossDeviceShowOff%5D=true&showUploadFirst=true&showProgressBar=true&appId=${appId}&testingUri=https%3A%2F%2Fverify.vouched.id%2F&crossDeviceQRCode=false&crossDeviceHandoff=false&crossDevice=false&crossDeviceSMS=false&id=both&face=both&liveness=straight&enableEyeCheck=false&debug=true&showFPS=false&sandbox=false&theme%5Bname%5D=verbose&theme%5Bfont%5D=Arial%2C%20Helvetica%2C%20sans-serif&theme%5BfontColor%5D=%23333&theme%5BiconLabelColor%5D=%23333&theme%5BbgColor%5D=%23FFF&theme%5BbaseColor%5D=%232E159F&theme%5BnavigationDisabledBackground%5D=rgba%28203%2C%20203%2C%20203%2C%200.15%29&theme%5BnavigationDisabledText%5D=%23888&theme%5BbaseColorLight%5D=rgb%28232%2C244%2C252%29&theme%5BprogressIndicatorTextColor%5D=%23000&type=id&survey=true&includeBackId=true&includeBarcode=true&disableCssBaseline=false&showTermsAndPrivacy=false&maxRetriesBeforeNext=0&idShowNext=0&handoffView%5BonlyShowQRCode%5D=false&locale=en&userConfirmation%5BconfirmData%5D=false&userConfirmation%5BconfirmImages%5D=false&isStage=true&manualCaptureTimeout=35000"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,8 +57,6 @@ class MainActivity : AppCompatActivity() {
             // remember to replace webAppUrl with the URL of your Camera App
             loadUrl(webappUrl)
         }
-
-
     }
 
     override fun onRequestPermissionsResult(
